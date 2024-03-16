2f36b7064277c2ccb9663e905a4e98c6a20d9d02
elly-hacen <elly.hacen@gmail.com>
2024-03-16 21:28:40 +0500

    Added is_active to list_display

--- core/apps/junatics/admin.py
+++ core/apps/junatics/admin.py
@@ -4,4 +4,4 @@ from core.apps.junatics.models import Junatics
 
 @admin.register(Junatics)
 class JunaticsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'is_favorite')
+    list_display = ('name', 'price', 'is_favorite', 'is_active')
