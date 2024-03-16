9e2872bea902511600a45429c7c8cbe4e3df0229
elly-hacen <elly.hacen@gmail.com>
2024-03-16 14:41:43 +0500

    Added views for Junatics

--- core/apps/junatics/views.py
+++ core/apps/junatics/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
+
+from core.apps.junatics.models import Junatics
+from django.views.generic import TemplateView, ListView
 
 
 class HomePageView(TemplateView):
     template_name = 'junatics/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['junatics'] = Junatics.objects.all()
+        return context
+
+
