4cbd66983b80a6e70cfdff2422a9885dc78b980d
elly-hacen <elly.hacen@gmail.com>
2024-03-16 21:27:56 +0500

    Added custom model manager

--- core/apps/junatics/models.py
+++ core/apps/junatics/models.py
@@ -5,6 +5,11 @@ def user_directory_path(instance, filename):
     return f'product_images/{instance.id}/{filename}'
 
 
+class JunaticsManager(models.Manager):
+    def get_queryset(self):
+        return super(JunaticsManager, self).get_queryset().filter(is_active=True)
+
+
 class Junatics(models.Model):
     name = models.CharField(max_length=256)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -12,9 +17,15 @@ class Junatics(models.Model):
                               verbose_name='Product Image:',
                               blank=True, null=True)
     is_favorite = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    # Custom Manager
+    objects = models.Manager()
+    junatics = JunaticsManager()
 
     class Meta:
         verbose_name = 'Junatics'
+        verbose_name_plural = 'Junatics'
 
     def __str__(self):
         return self.name
