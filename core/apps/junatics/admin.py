c9fe9c0e2d5be3b22c6047821ef05f40c381d984
elly-hacen <elly.hacen@gmail.com>
2024-03-27 16:01:24 +0500

    Added prepopulated_fields to model for automatic slug generation from name field

--- core/apps/junatics/admin.py
+++ core/apps/junatics/admin.py
@@ -5,3 +5,4 @@ from core.apps.junatics.models import Junatics
 @admin.register(Junatics)
 class JunaticsAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'is_favorite', 'is_active')
+    prepopulated_fields = {'slug': ('name',)}
