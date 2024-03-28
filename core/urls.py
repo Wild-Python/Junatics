6a791aa80f2070b03876284e125272827b778f8c
elly-hacen <elly.hacen@gmail.com>
2024-03-28 05:21:07 +0500

    Added blog app in base urls

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.apps.junatics.urls', namespace='junatics')),
+    path('blog/', include('core.apps.blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
